a1e8aed1a39e9760adbb8958ca85ec2e2b59d60a
poke30744 <poke30744@gmail.com>
2021-11-16 19:42:15 +0900

    add Jenkinsfile

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
-import setuptools
+import setuptools, os
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+build_number = os.getenv('BUILD_NUMBER') or '0'
+
 setuptools.setup(
     name="tsmarker",
-    version="0.0.9",
+    version=f"0.1.{build_number}",
     author="poke30744",
     author_email="poke30744@gmail.com",
     description="Mark MEPG TS clips by program/CM",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
-    packages=setuptools.find_packages(),
-     install_requires=[
+    url="https://github.com/poke30744/tsmarker/blob/main/README.md",
+    packages=setuptools.find_packages(exclude=['tests',]),
+    install_requires=[
+        'tscutter',
         'sklearn',
         'opencv-python',
         'pandas'
